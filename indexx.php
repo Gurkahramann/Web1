01c77bbe9548a8ba8bf40f7670d0ccb1c421acfe
Gurkahramann <100561567+Gurkahramann@users.noreply.github.com>
2022-05-03 16:02:16 +0300

    Add files via upload

---
+++ indexx.php
@@ -0,0 +1,25 @@
+<?php
+$username=$_POST['username'];
+$password=$_POST['password'];
+if(isset($_POST['submit']))
+{
+    if(!empty($username) && !empty($password))
+    {
+        if($username=='g211210028@sakarya.edu.tr' && $password=='g211210028')
+        {
+            echo "Hoşgeldiniz g211210028";
+        }
+        else
+        {
+            echo "Hatali Giris Yaptiniz";
+            header("Refresh: 3; url=anagiris.html"); 
+        }
+    }
+    else
+    {
+        echo "Lütfen veri giriniz";
+        header("Refresh: 3; url=anagiris.html"); 
+    }
+ 
+  
+}
