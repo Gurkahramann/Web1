ae221a7f82872536978c50d73068633ab32612d2
Gurkahramann <gurkan-kahraman@hotmail.com>
2022-05-14 23:56:04 +0300

    last commit

--- indexx.php
+++ indexx.php
@@ -7,26 +7,27 @@ if(isset($_POST['submit']))
     {
         if($username=='g211210028@sakarya.edu.tr' && $password=='g211210028')
         {
-            echo "Hoşgeldiniz g211210028";
+            echo "Hoşgeldiniz g211210028 Lütfen bekleyin ana sayfaya yönlendiriliyorsunuz";
+            header("Refresh: 3; url=https://gurkahramanwebsite.netlify.app/index.html"); 
         }
         else
         {
             echo "Hatali Giris Yaptiniz";
-            header("Refresh: 3; url=anagiris.html"); 
+            header("Refresh: 3; url=https://gurkahramanwebsite.netlify.app/anagiris.html"); 
         }
     }
     else if(empty($username))
     {
         echo "Lütfen Kullanıcı Adınızı Giriniz";
-        header("Refresh: 3; url=anagiris.html"); 
+        header("Refresh: 3; url=https://gurkahramanwebsite.netlify.app/anagiris.html"); 
     }else if(empty($password))
     {
         echo "Lütfen Şifrenizi Giriniz";
-        header("Refresh: 3; url=anagiris.html"); 
+        header("Refresh: 3; url=https://gurkahramanwebsite.netlify.app/anagiris.html"); 
     }else
     {
         echo "Lütfen Boş Bırakmayınız";
-        header("Refresh:3; url=anagiris.html");
+        header("Refresh:3; url=https://gurkahramanwebsite.netlify.app/anagiris.html");
     }
  
   
